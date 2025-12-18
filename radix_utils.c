0f6301ddaafe8c94593dd2b3431aa817cc567f7a
elise <elise62110@gmail.com>
2025-12-18 22:23:04 +0100

    commit radix tobinary

--- radix_utils.c
+++ radix_utils.c
@@ -46,57 +46,32 @@ int	count_nb(int a)
 	}
 	return(i);
 }
-
-// int	finbinary(int *a)
-// {
-// 	int	*b;
-// 	int	i;
-// 	int	nb;
-
-// 	nb = count_nb(*a);
-// 	b = (int *)malloc(sizeof(int)*(nb + 1));
-// 	while  (i != (nb + 1))
-// 	{
-// 		b[i] = a[i];
-// 		a = (int *)(a * 10);
-// 		i++;
-// 	}
-// 	return(*b);
-// }
-
-// int	tobinary(int a)
-// {
-// 	int	*binary;
-// 	int	nb;
-
-// 	nb = count_nb(a);
-// 	binary = (int *)malloc(sizeof(int)*(nb + 1));
-// 	while (a != 0)
-// 	{
-// 		printf("nb = %d \n", nb);
-// 		binary[nb] = a % 2;
-// 		printf("a = %d \n", a);
-// 		nb--;
-// 		a /= 2;
-// 	}
-// 	return(finbinary(binary));
-// }
-
-void	radix(t_list a, t_list b)
+int	tobinary(int n)
 {
-	int	mult;
 	int	result;
+	int	mult;
 
+	result = 0;
 	mult = 1;
-	while ()
+	if (n == 0)
+		return(0);
+	while (n > 0)
+	{
+		result = result + (n % 2 * mult);
+		mult *= 10;
+		n /= 2;
+	}
+	return (result);
 }
+
+
 
 int	main()
 {
 	int	a;
 	int	b;
 
-	a = 5;
+	a = 65;
 	printf("a decimal = %d \n", a);
 	b = tobinary(a);
 	printf("binaire = %d \n", b);
